a826835fef2a8db943d328ffb690437e6d47cf43
bhavani125 <girinenibhavani12@gmil.com>
2021-06-03 18:55:29 +0530

    storing day and daily waige

--- EmpWages.sh
+++ EmpWages.sh
@@ -8,7 +8,6 @@ workingDays=20
 maxHrsInMonth=100
 monthSalary=0
 totalWorkingHr=0
-
 function getWorkingHours() {
 	case $empCheck in
 		$isFullTimePresent)
@@ -23,14 +22,3 @@ function getWorkingHours() {
 	esac
 	echo $workingHr
 }
-
-for (( i=0; i<$workingDays && $totalWorkingHr<$maxHrsInMonth; i++ ))
-do
-	empCheck=$((RANDOM%3))
-	workingHr="$( getWorkingHours $empCheck )"
-	dailyWage[i]=$((workingHr*wagePerHour))
-	totalWorkingHr=$((totalWorkingHr+workingHr))
-done
-echo "Employee Daily wage : ${dailyWage[@]}"
-monthSalary=$((wagePerHour*totalWorkingHr))
-echo "Employee monthly wage : $monthSalary"

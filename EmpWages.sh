7f68283de8b8c3634984e44367e2f0d003f51ef5
bhavani125 <girinenibhavani12@gmil.com>
2021-06-03 17:55:57 +0530

    employeeispresentorabsent

--- EmpWages.sh
+++ EmpWages.sh
@@ -1,1 +1,10 @@
 echo "Welcome to Employee Wage computation problem"
+present=1
+absent=0
+empCheck=$((RANDOM%2))
+if [ $empCheck -eq $present ]
+then
+	echo "Employee present"
+else
+	echo "Employee absent"
+fi
